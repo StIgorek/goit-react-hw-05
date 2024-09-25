d6dde28df1a6666e5536d17b71cc350e118c3b3e
StIgorek <elitelifegroup@gmail.com>
2024-09-25 16:58:12 +0300

    add lazy loading

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,19 +2,25 @@ import { Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import css from "../App/App.module.css";
 import Navigation from "../Navigation/Navigation";
-import Home from "../../pages/Home/Home";
-import MoviesPage from "../../pages/MoviesPage/MoviesPage";
-import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
-import MovieCast from "../MovieCast/MovieCast";
-import MovieReviews from "../MovieReviews/MovieReviews";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+import Loader from "../Loader/Loader";
+
+const Home = lazy(() => import("../../pages/Home/Home"));
+const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("../../pages/MovieDetailsPage/MovieDetailsPage")
+);
+const NotFoundPage = lazy(() =>
+  import("../../pages/NotFoundPage/NotFoundPage")
+);
+const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
 
 export default function App() {
   return (
     <div className={css}>
       <Navigation />
 
-      <Suspense fallback={<div>Loading ...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<MoviesPage />} />
